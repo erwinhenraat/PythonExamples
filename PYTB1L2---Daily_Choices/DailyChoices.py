d833d7a25d36c6ffa68c34587e7323dec28160b5
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-09 14:39:41 +0200

    rename to DailyChoices.py

---
+++ PYTB1L2---Daily_Choices/DailyChoices.py
@@ -0,0 +1,9 @@
+print("Your alarm rings, do you GETUP or SNOOZE ?")
+choice = input()
+if choice == 'GETUP':
+    print("You get out of bed and feel fresh!")
+elif choice == 'SNOOZE':
+    print("Hmmmmmm zzzzzzzzzzzzzzzzzzzzzzzzzz")
+else:
+    print(choice, " wasn't a valid choice")
+
