6f7753d5d771e4d343d0e8d67fbb6ff4a9c24bfd
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-09 13:59:08 +0200

    move and rename files and folders

--- Choice.py
+++
@@ -1,8 +0,0 @@
-print("Please choose A or B")
-choice = input()
-if choice == 'A':
-    print("congratulations with your choice for A")
-elif choice == 'B':
-    print("hmmmm B was a douptfull choice")
-else:
-    print(choice, " wasn't a valid choice")
---
+++ ignore/ChoiceComplete.py
@@ -0,0 +1,35 @@
+print("Your alarm rings, do you GETUP or SNOOZE ?")
+choice = input()
+if choice == 'GETUP':
+    print("You get out of bed and feel fresh!")
+elif choice == 'SNOOZE':
+    print("Hmmmmmm zzzzzzzzzzzzzzzzzzzzzzzzzz")
+else:
+    print(choice, " wasn't a valid choice")
+
+print("Time for breakfast, do you eat BREAD or MEUSLI ?")
+choice = input()
+if choice == 'BREAD':
+    print("You eat a delicious piece of bread with cheese")
+elif choice == 'MEUSLI':
+    print("hmmmm MEUSLI was good with some ")
+else:
+    print(choice, " wasn't a valid choice")
+
+print("How do you travel to school? by BUS or BIKE ?")
+choice = input()
+if choice == 'BUS':
+    print("Don't forget to use a mouth protector.")
+elif choice == 'BIKE':
+    print("Zoooooooooooooooooooooooooooooommmmmmm!")
+else:
+    print(choice, " wasn't a valid choice")
+
+print("You arived in class. Where do you sit? in FRONT or in the BACK ?")
+choice = input()
+if choice == 'FRONT':
+    print("You sit in front of class so you can concentrate en follow the lesson.")
+elif choice == 'BACK':
+    print("You choose to sit in the back of the class so you feel a bit safer.")
+else:
+    print(choice, " wasn't a valid choice")
