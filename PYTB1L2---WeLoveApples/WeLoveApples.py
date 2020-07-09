2589dc657ae164d97444394bc5be7827ce33355e
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-09 16:00:34 +0200

    Example calculation structure for we love apples

---
+++ PYTB1L2---WeLoveApples/WeLoveApples.py
@@ -0,0 +1,21 @@
+trees               = 0 #hoeveel bomen zijn er in totaal?
+shadedTrees         = 0 #hoeveel bomen staan er in de schaduw?
+sunnyTrees          = 0 #hoeveel in de zon?
+
+shadeOutputModifier = 0 #hoeveel procent productie geeft een schaduwboom?
+
+sunnyTreeOutput     = 0 #hoeveel appels geeft 1 zonnige boom?
+shadedTreeOutput    = 0 #hoeveel appels geeft 1 schaduw boom?
+
+sunnyOutput         = 0 #hoeveel appels geven alle zonnige bomen? 
+shadedOutput        = 0 #hoeveel appels geven alle schaduw bomen?
+totalOutput         = 0 #hoeveel appels zijn er in totaal?
+
+owners              = 0 #met hoeveel mensen verdelen we de appels?
+
+eatCount            = 0 #hoeveel appels houden we over omdat ze niet eerlijk te verdelen zijn?
+sellableOutput      = 0 #hoeveel appels zijn er over en dus verkoopbaar?
+cut                 = 0 #hoeveel appels mag ik verkopen?
+
+print(cut)
+
