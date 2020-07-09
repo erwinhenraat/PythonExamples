d73a6d105f668c475f92ee65396699504e298e80
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-09 16:15:21 +0200

    Added round function to we love apples Example

--- PYTB1L2---WeLoveApples/WeLoveApples.py
+++ PYTB1L2---WeLoveApples/WeLoveApples.py
@@ -1,21 +1,25 @@
-trees               = 0 #hoeveel bomen zijn er in totaal?
-shadedTrees         = 0 #hoeveel bomen staan er in de schaduw?
-sunnyTrees          = 0 #hoeveel in de zon?
+import math
 
-shadeOutputModifier = 0 #hoeveel procent productie geeft een schaduwboom?
+#Vervang de 0-en met de juiste berekeningen
 
-sunnyTreeOutput     = 0 #hoeveel appels geeft 1 zonnige boom?
-shadedTreeOutput    = 0 #hoeveel appels geeft 1 schaduw boom?
+trees               = 0             #hoeveel bomen zijn er in totaal?
+shadedTrees         = math.ceil(0)  #hoeveel bomen staan er in de schaduw (afgerond naar boven)?
+sunnyTrees          = 0             #hoeveel in de zon?
 
-sunnyOutput         = 0 #hoeveel appels geven alle zonnige bomen? 
-shadedOutput        = 0 #hoeveel appels geven alle schaduw bomen?
-totalOutput         = 0 #hoeveel appels zijn er in totaal?
+shadeOutputModifier = 0             #hoeveel procent productie geeft een schaduwboom?
 
-owners              = 0 #met hoeveel mensen verdelen we de appels?
+sunnyTreeOutput     = 0             #hoeveel appels geeft 1 zonnige boom?
+shadedTreeOutput    = 0             #hoeveel appels geeft 1 schaduw boom?
 
-eatCount            = 0 #hoeveel appels houden we over omdat ze niet eerlijk te verdelen zijn?
-sellableOutput      = 0 #hoeveel appels zijn er over en dus verkoopbaar?
-cut                 = 0 #hoeveel appels mag ik verkopen?
+sunnyOutput         = 0             #hoeveel appels geven alle zonnige bomen? 
+shadedOutput        = 0             #hoeveel appels geven alle schaduw bomen?
+totalOutput         = 0             #hoeveel appels zijn er in totaal?
+
+owners              = 0             #met hoeveel mensen verdelen we de appels?
+
+eatCount            = 0             #hoeveel appels houden we over omdat ze niet eerlijk te verdelen zijn?
+sellableOutput      = 0             #hoeveel appels zijn er over en dus verkoopbaar?
+cut                 = 0             #hoeveel appels mag ik verkopen?
 
 print(cut)
 
