7b39ca8b35f77115b2ceb27c3e8d2ca3e0c59978
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-09 10:04:29 +0200

    created example for input and conditional

---
+++ Choice.py
@@ -0,0 +1,8 @@
+print("Please choose A or B")
+choice = input()
+if choice == 'A':
+    print("congratulations with your choice for A")
+elif choice == 'B':
+    print("hmmmm B was a douptfull choice")
+else:
+    print(choice, " wasn't a valid choice")
