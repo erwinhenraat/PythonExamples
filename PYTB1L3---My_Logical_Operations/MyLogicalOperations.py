798d47570368dc1e067b265ef30fd8a0a0024257
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-09 15:06:58 +0200

    added MyLogicalOperations.py as an assignment

---
+++ PYTB1L3---My_Logical_Operations/MyLogicalOperations.py
@@ -0,0 +1,22 @@
+
+name = "erwin henraat"
+job = "teacher"
+moneyInAccount = 1300
+
+#Vervang de ** met de logische operatoren 'and' en/of 'or'
+
+#Zorg dat de if statement de functie buyABrandNewMotorcycle uitvoert als:
+# Mijn naam erwin henraat is en ik een baan heb.
+# Of als ik meer dan 10000 euro op mijn rekening heb staan.
+
+
+if name == "erwin henraat" ** job != None ** moneyInAccound > 10000:
+    buyABrandNewMotorcycle()
+
+
+def buyABrandNewMotorcycle():
+    for index in range(100):
+        print(":)")
+
+
+#Maak nu voor jezelf ook een logische voorwaarde waarin je de operatoren 'and' en 'or' gebruikt.
