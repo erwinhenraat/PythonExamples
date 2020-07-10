66b7265cc624e0fd4fe8e6d1cc2fb34a958d07cb
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-10 16:12:57 +0200

    added deeply nested example

---
+++ PYTB1L3---DeeplyNested/DeeplyNested.py
@@ -0,0 +1,17 @@
+#example of nested if statements
+if !corona:
+    if greatWeather:
+        if money > 3000:
+            if familyWants:
+                if iWant:
+                    goCamp()
+        else:
+            goRideBike()
+
+#nested if statements with logical operators
+if !corona and greatWeather:
+    if money > 3000:
+        if familyWants and iWant:
+            goCamp()
+    else:
+        goRideBike()
