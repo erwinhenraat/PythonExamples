a940e565f0c4647289c5665da0dcfc5dfbf23fd0
Erwin Henraat <e.henraat@ma-web.nl>
2020-07-10 16:19:10 +0200

    added deeply nested example

--- PYTB1L3---DeeplyNested/DeeplyNested.py
+++ PYTB1L3---DeeplyNested/DeeplyNested.py
@@ -1,3 +1,16 @@
+def goCamp():
+    print("ejoying the great outdoors!")
+def goRideBike():
+    print("also enjoying the great outdoors but at an increased pace")
+
+
+corona = False
+greatWeather = True
+money = 0
+familyWants = False
+iWant = True
+
+
 #example of nested if statements
 if !corona:
     if greatWeather:
