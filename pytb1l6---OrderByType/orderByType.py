9f6f7362a50007657d104c66de5cf45c614a5d19
Erwin Henraat <e.henraat@ma-web.nl>
2020-09-25 14:07:51 +0200

    order by type added

---
+++ pytb1l6---OrderByType/orderByType.py
@@ -0,0 +1,5 @@
+stuff = ['c',5645,9393.77,"hello", True, False, "Good morning",88, -90, 777.777, 90,665.33,"F"]
+
+if type(stuff[0]) is str:   #type(stuff[0]) geeft het datatype van het eerste item in de list 
+    print("string found")
+
